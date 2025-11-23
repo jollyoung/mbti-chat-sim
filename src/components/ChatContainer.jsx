48797aa55f33fb58ecbc8807c3f5460dda528ecb
S_Joseph <ssyakmu@gmail.com>
2025-11-24 08:44:43 +0900

    카톡형 UI 테스트 1

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -1,13 +1,30 @@
+// src/components/ChatContainer.jsx
 import ChatBubble from "./ChatBubble";
+import "../index.css";
 
 export default function ChatContainer({ messages }) {
-  if (!Array.isArray(messages)) return null;
-
   return (
-    <div className="chat-container">
-      {messages.map((msg, i) => (
-        <ChatBubble key={i} {...msg} />
-      ))}
+    <div className="chat-wrapper">
+
+      {/* 🔥 프로필 + 상단 헤더 */}
+      <div className="chat-header">
+        <img
+          src="/profile_npc.png"
+          className="npc-avatar"
+          alt="NPC Profile"
+        />
+        <div className="npc-info">
+          <div className="npc-name">상대방</div>
+          <div className="npc-status">온라인</div>
+        </div>
+      </div>
+
+      {/* 🔥 채팅 본문 */}
+      <div className="chat-body">
+        {messages.map((msg, i) => (
+          <ChatBubble key={i} role={msg.role} text={msg.text} />
+        ))}
+      </div>
     </div>
   );
 }
