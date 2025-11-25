44e16c0c7dfdbd0414b4ea32b3ec86e3e2ee9425
S_Joseph <ssyakmu@gmail.com>
2025-11-25 09:12:21 +0900

    ESTJ 제외 baseScenario

--- src/hooks/storyEngine.jsx
+++ src/hooks/storyEngine.jsx
@@ -1,25 +1,36 @@
-import baseScenario from "../stories/base/baseScenario.js";
+import INFP from "../stories/INFP.js";
+import INFJ from "../stories/INFJ.js";
+import INTJ from "../stories/INTJ.js";
+import INTP from "../stories/INTP.js";
+import ISFP from "../stories/ISFP.js";
+import ISFJ from "../stories/ISFJ.js";
+import ISTP from "../stories/ISTP.js";
+import ISTJ from "../stories/ISTJ.js";
+import ENFP from "../stories/ENFP.js";
+import ENFJ from "../stories/ENFJ.js";
+import ENTP from "../stories/ENTP.js";
+import ENTJ from "../stories/ENTJ.js";
+import ESFP from "../stories/ESFP.js";
+import ESFJ from "../stories/ESFJ.js";
+import ESTP from "../stories/ESTP.js";
 import ESTJ from "../stories/ESTJ.js";
 // ... MBTI 16개 파일 import
 
 export const storyTable = {
-  // INFP,
-  // INFJ,
-  // INTJ,
-  // INTP,
-  // ISFP,
-  // ISFJ,
-  // ISTP,
-  // ISTJ,
-  // ENFP,
-  // ENFJ,
-  // ENTP,
-  // ENTJ,
-  // ESFP,
-  // ESFJ,
-  // ESTP,
-  ESTJ,
-
-  // 없는 MBTI는 자동으로 baseScenario 사용하도록 fallback
-  DEFAULT: baseScenario
+  INFP,
+  INFJ,
+  INTJ,
+  INTP,
+  ISFP,
+  ISFJ,
+  ISTP,
+  ISTJ,
+  ENFP,
+  ENFJ,
+  ENTP,
+  ENTJ,
+  ESFP,
+  ESFJ,
+  ESTP,
+  ESTJ
 };
---
+++ src/stories/ISTJ.js
@@ -0,0 +1,18 @@
+const baseScenario = {
+  intro: [
+    { role: "npc", text: "안녕하세요 :)" },
+    {
+      type: "choice",
+      question: "답장하기",
+      options: [
+        { label: "반가워요!", next: "start1" },
+        { label: "누구세요?", next: "start1" }
+      ]
+    }
+  ],
+  start1: [
+    { role: "npc", text: "이야기 나눠보고 싶어서 연락드렸어요." }
+  ]
+};
+
+export default baseScenario;
