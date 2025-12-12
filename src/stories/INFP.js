6500238c91f6f162e5b7be670a3c439bb1582de6
S_Joseph <ssyakmu@gmail.com>
2025-12-12 10:07:59 +0900

    프롤로그 적용 수정

--- src/stories/INFP.js
+++ src/stories/INFP.js
@@ -6,7 +6,7 @@ const INFP = {
   // 00. 프로필 소개 (프롤로그)
   [SCENE.PROLOGUE]: [
     {
-      role: "system",
+      role: "player",
       text:
         "✨ 오늘의 매칭 상대 프로필\n\n" +
         "이름: 서지은\n" +
